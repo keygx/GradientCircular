1a3325a9e91b1723b18d00241d05afc83963dd5f
keygx <y.kagiyama@gmail.com>
2018-06-20 19:45:47 +0900

    Refactor UIGraphicsImageRenderer

--- GradientCircular/Extensions/UIView+Ex.swift
+++ GradientCircular/Extensions/UIView+Ex.swift
@@ -13,12 +13,29 @@ extension UIView {
         if alpha < 1.0 {
             self.alpha = alpha
         }
+        
         let scale = UIScreen.main.scale
         UIGraphicsBeginImageContextWithOptions(self.frame.size, false, scale)
         self.layer.render(in: UIGraphicsGetCurrentContext()!)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
         return image!
+        
+        /***
+         CGImageMaskCreate: invalid mask bits/component: 16.
+         */
+        /*if #available(iOS 10.0, *) {
+            let renderer = UIGraphicsImageRenderer(size: self.bounds.size)
+            return renderer.image { rendererContext in
+                self.layer.render(in: rendererContext.cgContext)
+            }
+        } else {
+            let scale = UIScreen.main.scale
+            UIGraphicsBeginImageContextWithOptions(self.frame.size, false, scale)
+            self.layer.render(in: UIGraphicsGetCurrentContext()!)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return image!
+        }*/
     }
 }
